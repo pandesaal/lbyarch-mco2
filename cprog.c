412dd5e6cf7e282cfa65a773c63e96b0fc1edfca
sunny ☆ <146058145+pandesaal@users.noreply.github.com>
2025-11-17 15:06:33 +0800

    c program starting

--- cprog.c
+++ cprog.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
-extern void hewo();
+//extern void hewo(); // asm function
+//int main() {
+//	
+//	hewo();
+//	return 0;
+//}
+
+extern float sdotComp(int size);
+
 int main() {
 	
-	hewo();
+	int n = 0;
+	float result = 0;
+	
+	printf("Enter length of vector (int): ");
+	scanf("%d", &n);
+	
+	result = sdotComp(n);
+	printf("Dot product: %f\n", result);
+	
 	return 0;
 }
