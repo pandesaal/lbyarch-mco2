e8ef5ec894afd4ca412cd84be7a5bbee19a236a0
sunny ☆ <146058145+pandesaal@users.noreply.github.com>
2025-11-21 15:44:19 +0800

    init arrays, sanity check
    Added functions for array initialization and sanity check.

--- cprog.c
+++ cprog.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-extern float sdotComp(int size);
+#define ARRMAX 1073741824
+#define SANITY 1
+
+extern float sdotComp(float *array1, float *array2, int size);
+
+float* initArr() {
+	float *arr = (float *)malloc(sizeof(float) * ARRMAX);
+	if (arr == NULL) {
+        printf("Memory Allocation Failed\n");
+        exit(0);
+    }
+    
+    for (int i = 0; i < ARRMAX; i++){
+    	arr[i] = 2;
+	}
+	
+	return arr;
+}
+
+void sanityCheck(float *array1, float *array2, int size){
+	float res = 0;
+	for (int i = 0; i < size; i++){
+		res += array1[i]*array2[i];
+	}
+	
+	printf("Dot product (C check):	 %f\n", res);
+}
 
 int main() {
+	
+	float *arr = initArr();
 	
 	int n = 0;
 	float result = 0;
@@ -10,8 +40,10 @@ int main() {
 	printf("Enter length of vector (int): ");
 	scanf("%d", &n);
 	
-	result = sdotComp(n);
-	printf("Dot product: %f\n", result);
+	result = sdotComp(arr, arr, n);
+	printf("Dot product (ASM check): %f\n", result);
+	if (SANITY) sanityCheck(arr, arr, n);
 	
+	free(arr);
 	return 0;
 }
