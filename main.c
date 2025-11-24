e225a8c31d2f7ef69c0274c643094dccdfc1ba72
sunny ☆ <146058145+pandesaal@users.noreply.github.com>
2025-11-24 13:11:03 +0800

    better main file

--- main.c
+++ main.c
@@ -3,45 +3,52 @@
 #include <time.h>
 #include "kernel.c"
 
-#define REPEAT 30
-
 int main() {
 	
 	float *arr = initArr();
 	
-	int choice = 0;
-	int n = 0
+	char choice = '0';
+	int n = 0;
 	float result = 0;
 	
-	printf("[1] - [4]: input sizes for vectors\n")
-	printf("[1] 2^20\n");
-	printf("[2] 2^24\n");
-	printf("[3] 2^30\n");
-	printf("[4] Custom length\n");
-	printf("[5] Exit\n");
-	printf("Enter choice: ");
-	scanf("%d", &choice);
-	
-	switch (choice){
-		case 1:
-			n = ARRMAX20;
-			break;
-		case 2:
-			n = ARRMAX24;
-			break;
-		case 3:
-			n = ARRMAX30;
-			break;
-		case 4:
-			printf("Enter length of vector: ");
-			scanf("%d", &n);
-		default:
-			printf("")
+	while (choice != '5') {
+		
+		printf("[1] - [4]: input sizes for vectors\n");
+		printf("[1] 2^20\n");
+		printf("[2] 2^24\n");
+		printf("[3] 2^30\n");
+		printf("[4] Custom length\n");
+		printf("[5] Exit\n");
+		printf("Enter choice: ");
+		scanf("%c", &choice);
+		
+		switch (choice){
+			case '1':
+				n = ARRMAX20;
+				run(arr, n);
+				break;
+			case '2':
+				n = ARRMAX24;
+				run(arr, n);
+				break;
+			case '3':
+				n = ARRMAX30;
+				run(arr, n);
+				break;
+			case '4':
+				printf("Enter length of vector: ");
+				scanf("%d", &n);
+				if (n > 0) run(arr, n);
+				else printf("Invalid input, please try again.\n");
+				break;
+			case '5':
+				printf("Exiting program...\n");
+				break;
+			default:
+				printf("Invalid input, please try again.\n");
+				break;
+		}
 	}
-	
-	result = sdotComp(arr, arr, n);
-	printf("Dot product (ASM check): %f\n", result);
-	if (SANITY) sanityCheck(arr, arr, n);
 	
 	free(arr);
 	return 0;
